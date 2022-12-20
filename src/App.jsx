77ca51e059f8ad8f9017339c3592ea688522fc1c
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-12-20 21:23:29 +0000

    commit before shutting pc off

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.scss";
 import Characters from "./Container/Characters/Characters";
+import {useState, useEffect}
 
 const App = () => {
+  const getCharacters = async () => {
+    const response = await fetch(
+      "https://bobsburgers-api.herokuapp.com/characters/?limit=9&skip=432"
+    );
+    const data = await response.json();
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
   return (
     <div className="App">
       <h1> Bob's Burgers </h1>
