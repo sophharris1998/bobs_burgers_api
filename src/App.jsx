f18c8f1350a86f768b46a2828c2e3fa1bc5b0c72
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2022-12-20 18:42:03 +0000

    character card

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.scss";
+import Characters from "./Container/Characters/Characters";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1> Bob's Burgers </h1>
+      <Characters />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.scss";
+
+const Card = () => {
+  return (
+    <div>
+      <h2> Name </h2>
+      <img src="" alt="" />
+      <h3> Gender </h3>
+      <h3> Hair Color </h3>
+    </div>
+  );
+};
+
+export default Card;
